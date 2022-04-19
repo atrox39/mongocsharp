e17bcaeb85d7600aac1286a8c5f444d5fd32eac8
Eddy Ortega <cofter11@gmail.com>
2022-04-18 21:10:32 -0700

    MongoDB C# - Net Core

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace mongocsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient dbClient = new MongoClient("mongodb://localhost/");
+            IMongoDatabase database = dbClient.GetDatabase("mongocsharp");
+            IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>("testCollection");
+            BsonDocument document = new BsonDocument("message", "hello");
+            collection.InsertOne(document);
+            var firstDocument = collection.Find(new BsonDocument()).FirstOrDefault();
+            Console.WriteLine(firstDocument.ToString());
+        }
+    }
+}
